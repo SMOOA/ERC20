8dd82a7e553222f8468df383d5d79808c0de01d3
SMOOA DevTeam <44188758+SMOOA@users.noreply.github.com>
2018-10-16 14:12:47 +0700

    Create SMOOA_Mintable.sol

---
+++ SMOOA_Mintable.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @dev ERC20 minting logic
+ */
+contract SMOOAMintable is ERC20, MinterRole {
+  /**
+   * @dev Function to mint tokens
+   * @param to The address that will receive the minted tokens.
+   * @param value The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(
+    address to,
+    uint256 value
+  )
+    public
+    onlyMinter
+    returns (bool)
+  {
+    _mint(to, value);
+    return true;
+  }
+}
